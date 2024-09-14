3a92c2b90367de39ea3d0548c77564d2c56c6578
Shobhit Kumar Sharma <sharmashobhit1000@gmail.com>
2024-09-15 00:47:36 +0530

    Final changes done

--- app.js
+++ app.js
@@ -11,17 +11,15 @@ app.set("view engine", "ejs");
 app.get("/", async (req, res) => {
   let transactions = await transactionModel.find({});
 
-
   let total = 0;
   transactions.forEach(function (transaction) {
     if (transaction.amount[0] === "-") {
-      total -= parseFloat(transaction.amount.slice(1))
+      total -= parseFloat(transaction.amount.slice(1));
     } else {
       total += parseFloat(transaction.amount);
     }
   });
-   total = parseFloat(total.toFixed(2));
-  
+  total = parseFloat(total.toFixed(2));
 
   res.render("home", { transactions, total });
 });
@@ -37,6 +35,21 @@ app.post("/transaction", async (req, res) => {
   });
 
   res.redirect("/");
+});
+
+app.get("/delete/:id", async (req, res) => {
+  try {
+    let transaction = await transactionModel.findOne({ _id: req.params.id });
+
+    if (transaction) {
+      await transactionModel.deleteOne({ _id: transaction._id });
+      res.redirect("/");
+    } else {
+      res.status(404).send("Transaction not found");
+    }
+  } catch (error) {
+    res.status(500).send("Error deleting transaction");
+  }
 });
 
 app.listen(3000);
